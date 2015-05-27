200fa2e6972e314991a18fef92d144db7b0f10b1
Marcial Soto <hola@marcialsoto.com>
2015-05-26 22:16:03 -0500

    Footer finalizado

---
+++ wp-content/uploads/sucuri/sucuri-failedlogins.php
@@ -0,0 +1,2 @@
+<?php exit(0); ?>
+{"user_login":"osinfor","user_password":"","attempt_time":1432693176,"remote_addr":"127.0.0.1","user_agent":"Mozilla\/5.0 (Windows NT 6.1; WOW64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/43.0.2357.65 Safari\/537.36"}
--- wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -4,3 +4,4 @@
 {"user_id":1,"user_login":"rv","user_remoteaddr":"127.0.0.1","user_hostname":"msoto-PC","user_lastlogin":"2015-05-12 21:23:02"}
 {"user_id":1,"user_login":"rv","user_remoteaddr":"127.0.0.1","user_hostname":"msoto-PC","user_lastlogin":"2015-05-13 21:28:08"}
 {"user_id":1,"user_login":"rv","user_remoteaddr":"127.0.0.1","user_hostname":"msoto-PC","user_lastlogin":"2015-05-13 21:31:48"}
+{"user_id":1,"user_login":"rv","user_remoteaddr":"127.0.0.1","user_hostname":"msoto-PC","user_lastlogin":"2015-05-26 21:19:53"}
